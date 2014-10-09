5ee3dab664ea7884ca4f976d2eff73d1be57eba4
Hack Reactor Students <accounts@hackreactor.com>
2014-10-08 20:46:59 -0700

    migrated db to mongodb apart from navlinks handler

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,40 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
-});
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function(){
+//     this.on('creating', function(model, attrs, options){
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
 
-module.exports = Link;
+var linkify = function(url, title, base_url){
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  var code = shasum.digest('hex').slice(0,5);
+  return new db.Urls({
+    url: url,
+    code: code,
+    title: title,
+    base_url: base_url
+  })
+};
+
+// var Link = mongoose.model('users', usersSchema);
+
+
+// module.exports = Link;
+
+exports.linkify = linkify;
+
+
+
--- app/models/user.js
+++ app/models/user.js
@@ -2,24 +2,57 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
-});
+// var User = db.Model.extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   initialize: function(){
+//     this.on('creating', this.hashPassword);
+//   },
+//   comparePassword: function(attemptedPassword, callback) {
+//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+//       callback(isMatch);
+//     });
+//   },
+//   hashPassword: function(){
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
+// });
 
-module.exports = User;
+var userify = function(username, password){
+
+  var hashed = bcrypt.hashSync(password,null);
+
+  return new db.Users({
+    username: username,
+    password: hashed,
+  });
+};
+
+var comparePassword = function(user,entryPassword,callback){
+  db.Users.findOne({username:user}, function(err,result){
+    if (err) {
+      console.log('error accessing db');
+    }
+    console.log(result);
+    console.log(result.password);
+    bcrypt.compare(entryPassword, result.password, function(err,result){
+      if (err) {
+        console.log('Error comparing passwords',err);
+      } else {
+        console.log(result, "password compare result");
+        callback(result);
+      }
+    });
+  });
+};
+
+
+exports.userify = userify;
+exports.comparePassword = comparePassword;
+
+
+
